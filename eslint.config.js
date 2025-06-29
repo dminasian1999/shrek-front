73fb197fb5038f63f1ea2c3512b64d52ac5df6f7
LENOVO <d.minasian1999@gmail.com>
2025-06-29 03:46:06 +0300

    4th

--- eslint.config.js
+++ eslint.config.js
@@ -1,70 +1,28 @@
-import js from "@eslint/js"
-import vitestPlugin from "@vitest/eslint-plugin"
-import prettierConfig from "eslint-config-prettier/flat"
-import reactPlugin from "eslint-plugin-react"
-import reactHooksPlugin from "eslint-plugin-react-hooks"
-import globals from "globals"
-import { config, configs } from "typescript-eslint"
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
 
-const eslintConfig = config(
-    {
-      name: "global-ignores",
-      ignores: [
-        "**/*.snap",
-        "**/dist/",
-        "**/.yalc/",
-        "**/build/",
-        "**/temp/",
-        "**/.temp/",
-        "**/.tmp/",
-        "**/.yarn/",
-        "**/coverage/",
+export default tseslint.config(
+  { ignores: [''] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
       ],
     },
-    {
-      name: `${js.meta.name}/recommended`,
-      ...js.configs.recommended,
-    },
-    configs.strictTypeChecked,
-    configs.stylisticTypeChecked,
-    vitestPlugin.configs.recommended,
-    {
-      name: "eslint-plugin-react/jsx-runtime",
-      ...reactPlugin.configs.flat["jsx-runtime"],
-    },
-    reactHooksPlugin.configs["recommended-latest"],
-    {
-      name: "main",
-      linterOptions: {
-        reportUnusedDisableDirectives: 2,
-      },
-      languageOptions: {
-        ecmaVersion: 2020,
-        globals: globals.browser,
-        parserOptions: {
-          projectService: true,
-          tsconfigRootDir: import.meta.dirname,
-        },
-      },
-      settings: {
-        vitest: {
-          typecheck: true,
-        },
-      },
-      rules: {
-        "@typescript-eslint/consistent-type-definitions": [2, "type"],
-        "@typescript-eslint/consistent-type-imports": [
-          2,
-          {
-            prefer: "type-imports",
-            fixStyle: "separate-type-imports",
-            disallowTypeAnnotations: true,
-          },
-        ],
-      },
-    },
-
-    prettierConfig,
+  },
 )
-
-export default eslintConfig
