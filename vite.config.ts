bd786dca1bf0aa11cc4ca81277c046aea019615b
LENOVO <d.minasian1999@gmail.com>
2025-07-27 23:57:38 +0300

    render fixed3

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // base: '/otis-kit-pro/',
   build: {
     outDir: 'dist'
   },
   server: {
-    // host: true,
-    // port: 8080,
+    host: true,
+    port: 8080,
     open: true
   }
+
 });
